3f59e2922af187abeccf3dd62c6e75166d4d88cf
Luigi Cisana <github@cisana.net>
2016-10-16 16:16:27 +0200

    update: new search profile node.

--- ramp-thermostat/ramp-thermostat.js
+++ ramp-thermostat/ramp-thermostat.js
@@ -3,44 +3,19 @@ module.exports = function(RED) {
   
   function Profile(n) {
     RED.nodes.createNode(this,n);
+    this.n = n;
     this.name = n.name;
-    var timei, tempi, arr, minutes;
-    this.points = {};
-    
-    var points_str = '{';
-    
-    for (var i=1; i<=10; i++) {
-      timei = "time"+i;
-      tempi = "temp"+i;
-      if (typeof(n[timei]) !== "undefined" && n[timei] !== "") {
-        arr = n[timei].split(":");
-        minutes = parseInt(arr[0])*60 + parseInt(arr[1]);
-        points_str += '"' + minutes + '":"' + n[tempi] + '",'; 
-      }
-    }
-    points_str = points_str.slice(0,points_str.length-1);
-    points_str += '}';
-    
-    //console.log(points_str);
-    
-    this.points = JSON.parse(points_str);
   }
   RED.nodes.registerType("profile",Profile);
   
   function RampThermostat(config) {
     RED.nodes.createNode(this, config);
-       
-    this.config = config;
-    
+           
     this.profile = RED.nodes.getNode(config.profile);
-    //this.warn(JSON.stringify(this.profile.points));
+    this.points = getPoints(this.profile.n);
     
-    var globalContext = this.context().global;
-    globalContext.set(this.profile.name, config.profile);
     
     this.on('input', function(msg) {
-    
-      //this.warn(JSON.stringify(this.config.profile));
       
       var msg1 = {"topic":"state"};
       var msg2 = {"topic":"actual"};
@@ -50,26 +25,39 @@ module.exports = function(RED) {
       
       var date = new Date();
       var actual_mins = date.getHours()*60 + date.getMinutes();
-      
+ 
       if (typeof msg.profile !== "undefined") {
-        var profileId = globalContext.get(msg.profile);
-        if (typeof profileId === "undefined") {
+      
+        var found = false;
+        //var count = 0;
+        
+        RED.nodes.eachNode(function(n) {
+          if (n.type === "profile" && n.name === msg.profile) {
+            this.profile = n;
+            this.points = getPoints(n);
+            found = true;
+          }
+          //count++;
+        }.bind(this));
+      
+        //this.warn("count " + count);
+        
+        if (found) {
+          this.status({fill:"green",shape:"dot",text:"profile set to "+this.profile.name});
+        } else {
           this.warn(msg.profile+" not found!");
           this.status({fill:"red",shape:"dot",text:msg.profile+" not found!"});
-        } else {
-          this.profile = RED.nodes.getNode(profileId);
-          this.status({fill:"green",shape:"dot",text:"profile = "+this.profile.name});
         }
       }
-    
-      //this.warn("name " + this.profile.name + " points " + JSON.stringify(this.profile.points));
 
       if (typeof msg.payload !== "undefined") {
-        for (var k in this.profile.points) {
+        
+        //this.warn("name " + this.profile.name + " points " + JSON.stringify(this.points));
+        for (var k in this.points) {
           point_mins = parseInt(k);
-          //this.warn("mins " + point_mins + " temp " + this.profile.points[k]);
+          //this.warn("mins " + point_mins + " temp " + this.points[k]);
           
-          point_target = parseFloat(this.profile.points[k]);
+          point_target = parseFloat(this.points[k]);
           
           if (actual_mins < point_mins) {
             gradient = (point_target - pre_target) / (point_mins - pre_mins);
@@ -104,4 +92,29 @@ module.exports = function(RED) {
     });
   }
   RED.nodes.registerType("ramp-thermostat",RampThermostat);
+
+  
+  function getPoints(n) {
+    var timei, tempi, arr, minutes;
+    var points = {};
+    
+    var points_str = '{';
+    
+    for (var i=1; i<=10; i++) {
+      timei = "time"+i;
+      tempi = "temp"+i;
+      if (typeof(n[timei]) !== "undefined" && n[timei] !== "") {
+        arr = n[timei].split(":");
+        minutes = parseInt(arr[0])*60 + parseInt(arr[1]);
+        points_str += '"' + minutes + '":"' + n[tempi] + '",'; 
+      }
+    }
+    points_str = points_str.slice(0,points_str.length-1);
+    points_str += '}';
+    
+    //console.log(points_str);
+    
+    points = JSON.parse(points_str);
+    return points;
+  }
 }
